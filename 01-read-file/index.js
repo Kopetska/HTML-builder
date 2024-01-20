b561a3f40751bbc37e1a31119b7cd424ef594237
Anna Kopetska <a.laptieva@icloud.com>
2024-01-20 19:28:16 +0200

    feat:finish first task read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const fs = require('fs');
+const stream = fs.createReadStream(
+  path.join('01-read-file', 'text.txt'),
+  'utf-8',
+);
+
+stream.on('data', (chunk) => console.log(chunk));
